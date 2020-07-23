9e0198b059fb895806172aba764c342bf10fa0e3
Eden (Nayul) Kim <59883982+Edennyk@users.noreply.github.com>
2020-07-23 15:18:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ const clockContainer = document.querySelector(".clock-container"),
 // get current time
 function getTime(){
     const today = new Date();
-    const hour = today.getHours(),
+    const hours = today.getHours(),
          minutes= today.getMinutes();
-    clockTitle.innerHTML = `${hour}:${minutes}`;
+    clockTitle.innerHTML = `${hours < 10 ? `0${hours}`: hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+    // if minutes < 10 {`0${minutes}`} : ? mean if true
+    // : mean else => just minutes 
 }
 
 function init(){
