f12f0df3410c50b6d6cfdc0eb2dc5b91b1fdc6d7
Eden (Nayul) Kim <59883982+Edennyk@users.noreply.github.com>
2020-07-26 23:28:49 -0400

    fixed weather

--- weather.js
+++ weather.js
@@ -1,10 +1,16 @@
+const container = document.querySelector(".weather-container");
 const APT_KEY = '7feccdabe99537b1615293d551b59446';
 const COORDS = 'coords';
 
 function getWeather(lat, lng){
     // get date 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`).then(function(json){
-        
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`).then(function(response){
+        return response.json();
+    }).then(function(json){
+      const locationWeather = json.weather[0].main;
+      const temperature = json.main.temp;
+      const place = json.name;
+      container.innerText = `${locationWeather} ${temperature}°C ${place}`;
     });
 }
 
@@ -27,7 +33,7 @@ function handleGeoSuccess(position){
 }
 
 function handelGeoError(){
-
+    console.log("Can't access geo location");
 }
 
 // get location in web page
@@ -44,8 +50,6 @@ function loadCoords(){
         getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
-
-
 
 function init(){
     loadCoords();
